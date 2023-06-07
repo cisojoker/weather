6f0789c460c2bef764f18326dc219c669b6cd9f5
silence <103921455+cisojoker@users.noreply.github.com>
2023-06-08 00:52:39 +0530

    Initial commit

---
+++ src/client/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import AppRouter from "./router/appRouter.jsx";
+
+class App extends Component {
+  render() {
+    return <AppRouter {...this.props} />;
+  }
+}
+
+export default App;
